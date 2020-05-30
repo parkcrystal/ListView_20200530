23a5387850424b3f222947637213356f8e8fa2bb
parkcrystal <9jeworld@naver.com>
2020-05-30 16:01:36 +0900

    getView에서 실제 데이터 반영중

--- app/src/main/java/com/phis/listview_20200530/adapters/StudentAdapter.kt
+++ app/src/main/java/com/phis/listview_20200530/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import android.content.Context
 import android.view.LayoutInflater
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.phis.listview_20200530.R
 import com.phis.listview_20200530.datas.Student
 
@@ -21,6 +22,25 @@ class StudentAdapter(context: Context, resId: Int, list:List<Student>) : ArrayAd
         }
 
         val row = tempRow!!
+
+
+//        리턴 해주기 전에, 필요 데이터 세팅하고 리턴하도록.
+
+//         자리에 맞는 데이터 불러오기.
+        val data = mList.get(position)
+
+//        XML 에서 데이터가 뿌려질 뷰를 가져오자.
+        val nameAndAgeTxt = row.findViewById<TextView>(R.id.nameAndAgeTxt)
+        val genderTxt = row.findViewById<TextView>(R.id.genderTxt)
+
+//        상황에 맞는 데이터 세팅.
+        nameAndAgeTxt.text = data.name
+        if(data.isMale) {
+            genderTxt.text = "남성 수강생"
+        } else {
+            genderTxt.text = "여성 수강생"
+        }
+
 
         return row
 
