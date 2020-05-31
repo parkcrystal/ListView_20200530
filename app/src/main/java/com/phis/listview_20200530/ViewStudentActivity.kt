3c4ccbd4956c30ae2076007c32df9efb656fe433
parkcrystal <9jeworld@naver.com>
2020-05-31 10:31:19 +0900

    받아낸 학생정보 활용 데이터 출력

--- app/src/main/java/com/phis/listview_20200530/ViewStudentActivity.kt
+++ app/src/main/java/com/phis/listview_20200530/ViewStudentActivity.kt
@@ -3,6 +3,7 @@ package com.phis.listview_20200530
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.phis.listview_20200530.datas.Student
+import kotlinx.android.synthetic.main.activity_view_student.*
 
 class ViewStudentActivity : AppCompatActivity() {
 
@@ -14,5 +15,17 @@ class ViewStudentActivity : AppCompatActivity() {
 
 
         val student = intent.getSerializableExtra("student") as Student
+
+        
+        nameTxt.text = student.name
+        ageTxt.text = "${student.getKoreanAge(2020)}세(${student.birthYear}년생"
+        
+        
+        if(student.isMale) {
+            genderTxt.text  = "남성"
+        } else {
+            genderTxt.text = "여성"
+        }
+        
     }
 }
