3630a1dc17fb1e9129dba5ff834ab65a5f1e5d7f
parkcrystal <9jeworld@naver.com>
2020-05-31 10:08:12 +0900

    학생 정보 조회화면 추가.

---
+++ app/src/main/java/com/phis/listview_20200530/datas/ViewStudentActivity.kt
@@ -0,0 +1,12 @@
+package com.phis.listview_20200530.datas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStudentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_student)
+    }
+}
